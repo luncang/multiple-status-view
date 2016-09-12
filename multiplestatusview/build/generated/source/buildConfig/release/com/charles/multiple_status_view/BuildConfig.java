06cf0bc7e37d9f5b0eeccd9608c5a451b282fb30
luncang <luncang1988@163.com>
2016-09-12 17:38:12 +0800

    all

---
+++ multiplestatusview/build/generated/source/buildConfig/release/com/charles/multiple_status_view/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.charles.multiple_status_view;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = false;
+  public static final String APPLICATION_ID = "com.charles.multiple_status_view";
+  public static final String BUILD_TYPE = "release";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+}
